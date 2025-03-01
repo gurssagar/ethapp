75214a5fa524d552c653b3ceee97a59a387ba8b9
gurssagar <123202956+gurssagar@users.noreply.github.com>
2025-03-01 05:38:38 +0530

    comit

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat, FlatCompatOptions } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      ...FlatCompat.Off(), // This disables all ESLint rules
+    },
+  },
 ];
 
 export default eslintConfig;
