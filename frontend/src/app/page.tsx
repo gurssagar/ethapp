27815db27dba24c4029dd84a5c9391537a3e579a
gurssagar <123202956+gurssagar@users.noreply.github.com>
2025-06-03 07:51:11 +0530

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -57,17 +57,17 @@ export default function BackgroundBeamsDemo() {
 
   return (
     <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-      <div className="max-w-2xl mx-auto p-4 z-10"> {/* Added z-10 here to ensure form is above beams if they overlap */}
+      <div className="max-w-3xl mx-auto p-4 z-10"> {/* Added z-10 here to ensure form is above beams if they overlap */}
         <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
           Join the waitlist
         </h1>
         <p></p>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-          Welcome to MailJet, the best transactional email service on the web.
-          We provide reliable, scalable, and customizable email solutions for
-          your business. Whether you&apos;re sending order confirmations,
-          password reset emails, or promotional campaigns, MailJet has got you
-          covered.
+        GitFund lets you earn crypto for solving real GitHub issues.
+        Maintainers post bounties, you fix bugs or add features, and
+        get paid—straight to your wallet. Build your rep, grow your skills,
+        and get rewarded. Spots are limited—join the waitlist now and be 
+        first in line for the future of open-source!
         </p>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-4"> {/* Added mt-4 for spacing */}
